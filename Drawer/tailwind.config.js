940623825871a19b4bdf7ad1eb4f92dd098d00ee
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-12-05 08:41:13 +0500

    UPD

---
+++ Drawer/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./Components/**/*.{razor,razor.cs}", "./wwwroot/js/*.js", "./src/ts/*.ts"],
+    darkMode: "class",
+    theme: {
+        extend: {}
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: []
+}
