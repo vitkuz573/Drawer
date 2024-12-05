3ffb3e81eea5b1a1125a29eab25778b6335dfc90
Vitaly Kuzyaev <vitkuz573@gmail.com>
2024-12-05 08:09:52 +0500

    UPD

--- Drawer/wwwroot/js/drawing.js
+++ Drawer/wwwroot/js/drawing.js
@@ -49,11 +49,13 @@
     }
 
     /**
-     * Генерирует уникальный идентификатор для фигур.
+     * Генерирует уникальный идентификатор (UUID v4).
      * @returns {string} - Уникальный идентификатор.
      */
     function generateUniqueId() {
-        return 'shape-' + Math.random().toString(36).slice(2, 11);
+        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
     }
 
     /**
